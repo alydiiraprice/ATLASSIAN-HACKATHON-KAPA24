8e5d7543618dd94e48bfe501f0cfae73a812dfdb
Alydiira <z5419495@ad.unsw.edu.au>
2024-07-02 18:23:13 +1000

    new urls

--- bonzai/src/frontend/index.jsx
+++ bonzai/src/frontend/index.jsx
@@ -25,21 +25,36 @@ const App = () => {
     const newTotal = total + integer;
     setTotal(newTotal);
     setProgress(newTotal / 100);
+
+    // check if current progress reaches a certain point to change the image
+    if (newTotal >= 100) { 
+      // fully grown bonsai
+      setImageUrl('https://i.imgur.com/3C5DECQ.png');
+    } 
+    else if (newTotal >= 50 && newTotal < 100) { 
+      // small tree
+      // setImageUrl(''); TO DO
+    } else if (newTotal < 50 && newTotal >= 25) { // sapling
+      setImageUrl('https://i.imgur.com/BwbjQTM.png');
+    }
   }
+
+  // state to hold the image url
+  const [imageUrl, setImageUrl] = useState('https://i.imgur.com/BwbjQTM.png'); // seed TO DO
 
   return (
     <>
       
-      <Image src='https://i.imgur.com/3C5DECQ.png' alt="Seedling" size="small"/>
+      <Image src={imageUrl} alt="bonsai" size="small"/>
       
       
       <Stack space="space.200" alignInline="center">
       <ProgressBar ariaLabel={`Done: ${current} of ${total}`} value={progress} />
           <Text>Progress Tracker</Text>
           <Inline space="space.200">
-              <Button onClick={() => addInteger(10)}>$25</Button>
-              <Button onClick={() => addInteger(20)}>$50</Button>
-              <Button onClick={() => addInteger(20)}>$75</Button>
+              <Button onClick={() => addInteger(10)}>$10</Button>
+              <Button onClick={() => addInteger(25)}>$25</Button>
+              <Button onClick={() => addInteger(50)}>$50</Button>
           </Inline>
       </Stack>
       
